4944aed6fa0214c9bca99b7bf1ee28aea8f249a0
Shahid Akhtar <shahidakhtar016@gmail.com>
2021-04-08 22:04:43 +0530

    removed unnecessary methods

--- src/services/fakeMovieService.js
+++ src/services/fakeMovieService.js
@@ -75,12 +75,12 @@ export function getMovie(id) {
   return movies.find(m => m._id === id);
 }
 
-export function saveMovie(movie) {
-  let movieInDb = movies.find(m => m._id === movie._id) || {};
-  movieInDb.title = movie.title;
-  movieInDb.genre = genresAPI.genres.find(g => g._id === movie.genreId);
-  movieInDb.numberInStock = movie.numberInStock;
-  movieInDb.dailyRentalRate = movie.dailyRentalRate;
+// export function saveMovie(movie) {
+//   let movieInDb = movies.find(m => m._id === movie._id) || {};
+//   movieInDb.title = movie.title;
+//   movieInDb.genre = genresAPI.genres.find(g => g._id === movie.genreId);
+//   movieInDb.numberInStock = movie.numberInStock;
+//   movieInDb.dailyRentalRate = movie.dailyRentalRate;
 
   // if (!movieInDb._id) {
   //   movieInDb._id = randomstring({
@@ -93,8 +93,8 @@ export function saveMovie(movie) {
   //   movies.push(movieInDb);
   // }
 
-  return movieInDb;
-}
+//   return movieInDb;
+// }
 
 // export function deleteMovie(id) {
 //   let movieInDb = movies.find(m => m._id === id);
