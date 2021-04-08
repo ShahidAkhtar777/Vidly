a2aa30dc1f33be78fedc43f50a14b4ca42a0d0f2
Shahid Akhtar <shahidakhtar016@gmail.com>
2021-04-08 20:20:48 +0530

    deletion of movies added

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -5,30 +5,47 @@ class Movies extends Component {
     state = { 
         movies: getMovies()
      };
-    render() { 
-        return ( 
+
+    handleDelete = (movie) => {
+        const movies = this.state.movies.filter( m => m._id !== movie._id);
+        this.setState({movies : movies});
+        // in modern js if key and value pair are same we can then skip them
+        // this.setState({movies});
+    }
+
+    render() {
+        const { length: count } = this.state.movies;
+        if(count===0)
+            return <p> There are no movies in the database.</p>
+
+        return (
+            <React.Fragment>
+            <p> showing { count } no of movies from database.</p>
             <div>
-               <table class="table">
+               <table className="table">
                    <thead>
                        <tr>
                            <th scope="col">Title</th>
                            <th scope="col">Genre</th>
                            <th scope="col">Stock</th>
                            <th scope="col">Rate</th>
+                           <th />
                        </tr>
                    </thead>
                    <tbody>
                        {this.state.movies.map(movie => (
-                       <tr>
+                       <tr key={movie._id}>
                            <td>{movie.title}</td>
                            <td>{movie.genre.name}</td>
                            <td>{movie.numberInStock}</td>
                            <td>{movie.dailyRentalRate}</td>
+                           <td><button onClick={()=>this.handleDelete(movie)} className="btn btn-danger btn-sm">Delete</button></td>
                        </tr>
                        ))}
                    </tbody>
                </table>
             </div>
+            </React.Fragment>
          );
     }
 }
